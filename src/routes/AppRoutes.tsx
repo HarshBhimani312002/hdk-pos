88a486a79e311e6ba768faa60e28054673629546
Harsh Bhimani <bhimaniharsh31@gmail.com>
2026-07-15 15:51:53 +0530

    Day 4 - Authentication UI and Routing

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,11 +1,32 @@
+import { Routes, Route } from "react-router-dom";
+
+import Login from "../pages/auth/Login";
+import Register from "../pages/auth/Register";
 import Dashboard from "../pages/dashboard/Dashboard";
+import NotFound from "../pages/not-found/NotFound";
+
 import MainLayout from "../layouts/MainLayout";
 
 const AppRoutes = () => {
   return (
-    <MainLayout>
-      <Dashboard />
-    </MainLayout>
+    <Routes>
+
+      <Route path="/" element={<Login />} />
+
+      <Route
+        path="/dashboard"
+        element={
+          <MainLayout>
+            <Dashboard />
+          </MainLayout>
+        }
+      />
+
+      <Route path="/register" element={<Register />} />
+
+      <Route path="*" element={<NotFound />} />
+
+    </Routes>
   );
 };
 
